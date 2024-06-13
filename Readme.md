5b47d13c028f385ab532fab9722424f19155c191
wuyq <wuyq2023@outlook.com>
2024-06-13 09:08:39 +0800

    third day

--- Readme.md
+++ Readme.md
@@ -1,2 +1,18 @@
 - 第一天：服务端与消息编码，使用encoder和decoder来进行编解码，首先用发送option字段通知服务端，要使用哪种编解码器。然后发送Header-body格式消息，header中会有访问的服务和seq序列号，body里有访问的参数。
+> 用一个map，key是编码器的类型（例如：Json、Gob、Yaml），value是New函数，根据传入的编码器类型不同，所用的构造函数也不同（例如：NewJson、NewGob、NewYaml）
 - 第二天：高性能客户端，使用call来进行封装，表示一次rpc调用会话，包含了访问的服务方法、seq、参数、返回值。客户端首先建立TCP连接，发送option，根据返回的套接字conn构建rpc client，调用client call方法将访问的服务方法、参数封装起来，编码后发送给服务端，服务端在接收到后解码，然后构造响应头和包体，包体由调用的方法决定，然后编码发回客户端。客户端收到后解码，放入reply中。
+- 第三天：服务注册，一个服务器上会有很多的服务，很多方法，因此需要事先注册。服务是从结构体映射过来的，方法就是结构体的方法，在这里通过反射，可以获得结构体的value和type，通过type可以获取到结构体拥有的方法，包括方法的数量和入参回参。 
+```go
+var s Student
+typ := reflect.Typeof(s)
+NumMethod := typ.NumMethod()
+method := typ.Method(1)
+```
+> 方法属于结构体的类型，且方法名要大写，才可以被发现。
+ ```go
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+```
+在服务器端通过map保存服务注册的结果，用sync.Map去存。
--- server.go
+++ server.go
@@ -2,12 +2,13 @@ package geerpc
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"geerpc/codec"
 	"io"
 	"log"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -23,7 +24,19 @@ var DefaultOption = &Option{
 	CodecType:   codec.GobType,
 }
 
-type Server struct{}
+type Server struct {
+	serviceMap sync.Map
+}
+
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc: service already defined: " + s.name)
+	}
+	return nil
+}
+
+func Register(rcvr interface{}) error { return DefaultServer.Register(rcvr) }
 
 func NewServer() *Server {
 	return &Server{}
@@ -73,6 +86,8 @@ var invalidRequest = struct{}{}
 type request struct {
 	h            *codec.Header // header of request
 	argv, replyv reflect.Value // argv and replyv of request
+	mtype        *methodType
+	svc          *service
 }
 
 func (s *Server) serveCodec(cc codec.Codec) {
@@ -95,15 +110,27 @@ func (s *Server) serveCodec(cc codec.Codec) {
 	_ = cc.Close()
 }
 
-func (s *Server) readRequest(cc codec.Codec) (*request, error) {
-	header, err := s.readRequestHeader(cc)
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
 	if err != nil {
 		return nil, err
 	}
-	req := &request{h: header}
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil {
-		log.Println("rpc server: read argv err:", err)
+	req := &request{h: h}
+	req.svc, req.mtype, err = server.findService(h.ServiceMethod)
+	if err != nil {
+		return req, err
+	}
+	req.argv = req.mtype.NewArgv()
+	req.replyv = req.mtype.NewReplyv()
+
+	// make sure that argvi is a pointer, ReadBody need a pointer as parameter
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
+	}
+	if err = cc.ReadBody(argvi); err != nil {
+		log.Println("rpc server: read body err:", err)
+		return req, err
 	}
 	return req, nil
 }
@@ -127,9 +154,33 @@ func (s *Server) sendResponse(cc codec.Codec, header *codec.Header, body interfa
 	}
 }
 
-func (s *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
 	defer wg.Done()
-	log.Println(req.h, req.argv.Elem())
-	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
-	s.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+		return
+	}
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server: service/method request ill-formed: " + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server: can't find service " + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc server: can't find method " + methodName)
+	}
+	return
 }
